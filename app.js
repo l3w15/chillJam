a7af923a0adcc371ea7394b13c81b8f125290cbd
Hannah Lillis <hannahamanda16@gmail.com>
2018-03-20 11:51:08 +0000

    Setting up Express with a promise

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
+const express = require('express')
+const app = express()
+
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs')
 
 var Twitter = require('twitter');
 var config = require('./config.js');
 
-var T = new Twitter(config);
+app.get('/', (req,res) => res.render('index', function(err, html){
+  res.send(twitterReq())
+}));
 
-var params = {
-  q: '#giveaway -filter:retweets',
-  count: 20,
-  result_type: 'recent',
-  lang: 'en'
-}
+app.listen(3000, () => console.log('Listening on port 3000'))
 
-T.get('search/tweets', params, function(err, data, response) {
-  if(!err) {
-    for (var i = 0; i < data.statuses.length; i++) {
-      // console.log(data.statuses[i].text)
-      console.log(data.statuses[i].text)
-    }
-  } else {
-    console.log(err)
+function twitterReq(){
+
+  var T = new Twitter(config);
+  var result = [];
+
+  var params = {
+    q: '#giveaway -RT',
+    count: 20,
+    result_type: 'recent',
+    lang: 'en'
   }
-});
+
+  const prom = new Promise(function(resolve,reject) {
+
+    T.get('search/tweets', params, function(err, data, response) {
+      if(!err) {
+         for (var i = 0; i < data.statuses.length; i++) {
+          resolve(result.push( data.statuses[i].text ))
+        }
+      } else {
+        reject( result.push( err ) )
+      }
+    })
+    return prom.then((result) => {
+      return result
+    })
+  });
+};
