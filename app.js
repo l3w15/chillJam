ea96f00337a26a7d4a50784be13fe22fd49f1dd0
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-20 14:42:58 +0000

    Slight refactor

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 
 app.get('/', function (req,res) {
-  twitterReq().then(data => {
-    res.render('index', { tweets: data })
+  twitterReq().then(tweets => {
+    res.render('index', { tweets: tweets })
   })
 });
 
@@ -20,35 +20,30 @@ app.listen(3000, () => console.log('Listening on port 3000'))
 function twitterReq(){
 
   var T = new Twitter(config);
-  var result = [];
 
   var params = {
     q: '#giveaway -RT',
-    count: 20,
-    result_type: 'recent',
+    count: 100,
+    // result_type: 'recent',
     lang: 'en'
   }
 
   const prom = new Promise(function(resolve,reject) {
     T.get('search/tweets', params, function(err, data, response) {
-      if(!err) {
         resolve( function(){
-          var tweets = [];
-          for (var i = 0; i < data.statuses.length; i++) {
-            tweets.push(data.statuses[i].text)
-          }
+          var tweets = data.statuses.map(tweet => tweet.text)
           return tweets
-        })
-      } else {
-        reject(function(){ console.log("failure") })
-      }
-    })
+        });
+
+        reject(function(){ console.log("failure") });
+    });
   });
 
-  return prom.then((result) => {
-    // console.log(result.toString())
-    return result()
-  }).catch((rej) => { rej(); })
-
-
+  return prom
+    .then((resolve) => {
+      return result()
+    })
+    .catch((rej) => {
+      rej();
+    })
 };
