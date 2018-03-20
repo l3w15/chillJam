386c260c30be99928eac961d5a660a92f38cefc9
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-20 20:39:22 +0000

    Complete tests and implementation of tweetParser()

--- test/tweetParser.js
+++ test/tweetParser.js
@@ -5,3 +5,14 @@ chai.use(chaiAsPromised)
 chai.should()
 
 var tweetParser = require("../lib/tweetParser.js");
+var testJson = require("./sampleTweetSearchJson.js")
+
+describe("tweetParser", function () {
+  it("contains the tweet's urls", function() {
+    tweetParser(testJson.statuses[0]).url.should.equal("https://twitter.com/NASA/status/967824267948773377")
+  })
+
+  it("contains the tweet's text", function() {
+    tweetParser(testJson.statuses[0]).text.should.equal("From pilot to astronaut, Robert H. Lawrence was the first African-American to be selected as an astronaut by any na… https://t.co/FjPEWnh804")
+  })
+})
