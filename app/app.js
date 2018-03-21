c46d950a7a7a4471c39df9c878c2ef8fdab89084
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-21 16:39:37 +0000

    Complete Like button functionality

--- app/app.js
+++ app/app.js
@@ -4,11 +4,7 @@ const app             = express();
 const bodyParser      = require('body-parser');
 const twitterReq      = requestBuilder.twitterReq;
 const defineParams    = requestBuilder.defineParams;
-// const TweetParser     = require('../lib/tweetParser.js');
-// const tweetParser     = TweetParser.tweetParser;
-// const reqBuilder      = new requestBuilder.RequestBuilder;
-// const Twitter         = require('twitter');
-// const config          = require('../config.js');
+const likeReq       = requestBuilder.likeReq;
 
 var params;
 
@@ -17,9 +13,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  res.render('index')
-})
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 app.route('/tweets')
   .get(function(req, res) {
@@ -30,10 +26,13 @@ app.route('/tweets')
       .catch(reject => { console.log(reject); });
   })
   .post(function(req, res) {
-    console.log("success")
-    console.log(req.body.query);
     params = defineParams(req.body.query);
-    res.redirect('/tweets')
-  })
+    res.redirect('/tweets');
+  });
+
+app.post('/favourites', function(req, res) {
+  likeReq(req.body.id);
+  res.redirect('/tweets');
+});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
