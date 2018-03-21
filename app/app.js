68aa373c428678292e00a18297ccdeb46abe9466
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-21 11:37:50 +0000

    Add search query form

--- app/app.js
+++ app/app.js
@@ -1,33 +1,51 @@
-const express = require('express');
+const express     = require('express');
 const TweetParser = require('../lib/tweetParser.js');
-const app = express();
-const Twitter = require('twitter');
-const config = require('../config.js');
+const app         = express();
+const Twitter     = require('twitter');
+const config      = require('../config.js');
 const tweetParser = TweetParser.tweetParser;
+const bodyParser  = require('body-parser');
 
 app.engine('html', require('ejs').renderFile);
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  twitterReq()
-    .then(tweets => {
-      res.render('index', { tweets: tweets() });
-    })
-    .catch(reject => { console.log(reject); });
-});
+app.get('/', function(req, res){
+  res.render('index')
+})
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+var params;
 
-var twitterReq = function() {
-  var T = new Twitter(config);
+app.route('/tweets')
+  .get(function(req, res) {
+    twitterReq(params)
+      .then(tweets => {
+        res.render('tweets', { tweets: tweets() });
+      })
+      .catch(reject => { console.log(reject); });
+  })
+  .post(function(req, res) {
+    console.log("success")
+    console.log(req.body.query)
+    defineParams(req.body.query);
+    res.redirect('/tweets')
+  })
 
-  var params = {
-    q: '#giveaway -RT',
+var defineParams = function(query) {
+  params = {
+    q: '#giveaway ' + query + ' -RT',
     count: 100,
     result_type: 'recent',
     lang: 'en'
   };
+  console.log(params);
+  return params;
+}
+
+
+var twitterReq = function(params) {
+  var T = new Twitter(config);
 
   const prom = new Promise(function(resolve, reject) {
     T.get('search/tweets', params, function(err, data, response) {
@@ -40,3 +58,5 @@ var twitterReq = function() {
 
   return prom;
 };
+
+app.listen(3000, () => console.log('Listening on port 3000'));
