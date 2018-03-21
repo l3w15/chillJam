2bcc43dae0e7781576bb155f04637be254393e30
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-21 15:15:10 +0000

    refactor to extract defineParams and twitterReq from controller

--- app/app.js
+++ app/app.js
@@ -1,10 +1,16 @@
-const express     = require('express');
-const TweetParser = require('../lib/tweetParser.js');
-const app         = express();
-const Twitter     = require('twitter');
-const config      = require('../config.js');
-const tweetParser = TweetParser.tweetParser;
-const bodyParser  = require('body-parser');
+const express         = require('express');
+const requestBuilder  = require('../lib/requestBuilder.js');
+const app             = express();
+const bodyParser      = require('body-parser');
+const twitterReq      = requestBuilder.twitterReq;
+const defineParams    = requestBuilder.defineParams;
+// const TweetParser     = require('../lib/tweetParser.js');
+// const tweetParser     = TweetParser.tweetParser;
+// const reqBuilder      = new requestBuilder.RequestBuilder;
+// const Twitter         = require('twitter');
+// const config          = require('../config.js');
+
+var params;
 
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.json());
@@ -14,8 +20,6 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
   res.render('index')
 })
-
-var params;
 
 app.route('/tweets')
   .get(function(req, res) {
@@ -27,36 +31,9 @@ app.route('/tweets')
   })
   .post(function(req, res) {
     console.log("success")
-    console.log(req.body.query)
-    defineParams(req.body.query);
+    console.log(req.body.query);
+    params = defineParams(req.body.query);
     res.redirect('/tweets')
   })
-
-var defineParams = function(query) {
-  params = {
-    q: '#giveaway ' + query + ' -RT',
-    count: 100,
-    result_type: 'recent',
-    lang: 'en'
-  };
-  console.log(params);
-  return params;
-}
-
-
-var twitterReq = function(params) {
-  var T = new Twitter(config);
-
-  const prom = new Promise(function(resolve, reject) {
-    T.get('search/tweets', params, function(err, data, response) {
-      resolve(() => {
-        return tweetParser(data);
-      });
-      reject(() => { return err; });
-    });
-  });
-
-  return prom;
-};
 
 app.listen(3000, () => console.log('Listening on port 3000'));
