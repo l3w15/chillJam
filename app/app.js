bc95b1e65c56b6ee59776ebc1964cc4fc4c61e83
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-21 17:42:15 +0000

    added likeAll followAll and retweetAll automation

--- app/app.js
+++ app/app.js
@@ -5,6 +5,9 @@ const bodyParser      = require('body-parser');
 const twitterReq      = requestBuilder.twitterReq;
 const defineParams    = requestBuilder.defineParams;
 const likeReq       = requestBuilder.likeReq;
+const likeAllTweets = requestBuilder.likeAllTweets;
+const retweetAllTweets = requestBuilder.retweetAllTweets;
+const followAllUsers = requestBuilder.followAllUsers;
 
 var params;
 
@@ -21,7 +24,11 @@ app.route('/tweets')
   .get(function(req, res) {
     twitterReq(params)
       .then(tweets => {
-        res.render('tweets', { tweets: tweets() });
+        var tweets = tweets();
+        likeAllTweets(tweets);
+        retweetAllTweets(tweets);
+        followAllUsers(tweets);
+        res.render('tweets', { tweets: tweets });
       })
       .catch(reject => { console.log(reject); });
   })
@@ -30,9 +37,9 @@ app.route('/tweets')
     res.redirect('/tweets');
   });
 
-app.post('/favourites', function(req, res) {
-  likeReq(req.body.id);
-  res.redirect('/tweets');
-});
+// app.post('/favourites', function(req, res) {
+//   likeReq(req.body.id);
+//   res.redirect('/tweets');
+// });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
