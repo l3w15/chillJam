bef07cf0308ccf60540ee38eed80d8d5c62b5fc5
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-21 09:50:14 +0000

    minor cosmetic updates

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ module.exports = {
   consumer_secret: 'jmfEvw1IqIxTiWQb01DF4vvPtNTfIqhzRXp870Jt9E2SpyLZw9',
   access_token_key: '975703712957128704-yv4ExjygSv6iKgUpTSCSiYvtnL3U2hl',
   access_token_secret: 'x2rAmaKLuNkWv2A1p8C6VRk6x8vd27NElIEWa683TpU0D'
-}
+};
