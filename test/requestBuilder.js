150d1e4b937fb19b688f2eed4519f7102ce2cc30
carlostrapet <trapet_llamas@hotmail.com>
2018-03-22 14:50:19 +0000

    created passing test with Spy for likeAllTweets

--- test/requestBuilder.js
+++ test/requestBuilder.js
@@ -1,45 +1,47 @@
 var chai = require('chai');
 var expect = require('chai').expect;
 var chaiAsPromised = require('chai-as-promised');
+var spies = require('chai-spies');
 
 chai.use(chaiAsPromised);
+chai.use(spies);
 chai.should();
 
 // var testJson = require('./testData/sampleTweetSearchJson.js');
 const requestBuilder    = require("../lib/requestBuilder.js")
 const tParser           = require("../lib/tweetParser.js")
-const tweetParser       = tParser.tweetParser
+const tweetParser       = tParser.tweetParser;
 const singleTweetParser = tParser.singleTweetParser;
-const twitterReq        = requestBuilder.twitterReq
+const twitterReq        = requestBuilder.twitterReq;
 const defineParams      = requestBuilder.defineParams;
 const likeReq           = requestBuilder.likeReq;
 const likeAllTweets     = requestBuilder.likeAllTweets;
 const retweetAllTweets  = requestBuilder.retweetAllTweets;
 const followAllUsers    = requestBuilder.followAllUsers;
 
-
-describe('defineParams', function(){
-  it('defines the search parameters', function(){
+describe('defineParams', function() {
+  it('defines the search parameters', function() {
     expect(defineParams('chocolate')).to.be.a('object').with.property('count')
-    // expect(defineParams('chocolate')).to.have.property('count')
-  })
+  });
 });
 
 describe('twitterReq', function() {
- it('returns the correct tweet data', function(done) {
-   var promise = twitterReq({
-     q: '#competition'
-   });
-   // console.log(promise.then().resolve())
-   promise.then().should.eventually.have.property('statuses').notify(done);
- });
+  it('returns the correct tweet data', function(done) {
+    var promise = twitterReq({
+      q: '#competition'
+    });
+    promise.then().should.eventually.have.property('statuses').notify(done);
+  });
 });
 
-describe('likeAllTweets', function(){
-  it('likes all returned tweets', function(){
-    var tweet1 = {id: 1234}
-    var tweet2 = {id: 5678}
-    likeAllTweets([tweet1, tweet2])
-    expect(tweet1.liked).to.be.true
-  })
+describe('likeAllTweets', function() {
+  it('likes all returned tweets', function() {
+    var tweet1 = {id: 1234};
+    var tweet2 = {id: 5678};
+    let spy1 = chai.spy.on(tweet1, 'likeReq');
+    let spy2 = chai.spy.on(tweet2, 'likeReq');
+    likeAllTweets([tweet1, tweet2]);
+    expect(spy1).to.have.been.called;
+    expect(spy2).to.have.been.called;
+  });
 });
