7dcd6d42f0d8e7ab1044006e6d7550e04188a2b3
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-22 22:32:10 +0000

    Added twitter-ish colour CSS

--- app/app.js
+++ app/app.js
@@ -17,6 +17,7 @@ const twitter = new Twitter(config);
 var params;
 
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
