cf8ee1d612e93620848b3b47486fcc917fa7cfa5
Hannah Lillis <hannahamanda16@gmail.com>
2018-03-22 11:48:51 +0000

    Pass test for likeAllTweets

--- test/app.js
+++ test/app.js
@@ -10,13 +10,3 @@ var requestBuilder = require("../lib/requestBuilder.js")
 var tParser = require("../lib/tweetParser.js")
 var tweetParser = tParser.tweetParser
 var twitterReq = requestBuilder.twitterReq
-
-describe('twitterReq', function() {
- it('returns the correct tweet data', function(done) {
-   var promise = twitterReq({
-     q: '#competition'
-   });
-   // console.log(promise.then().resolve())
-   promise.then().should.eventually.have.property('statuses').notify(done);
- });
-});
--- test/requestBuilder.js
+++ test/requestBuilder.js
@@ -6,11 +6,11 @@ chai.use(chaiAsPromised);
 chai.should();
 
 // var testJson = require('./testData/sampleTweetSearchJson.js');
-const requestBuilder      = require("../lib/requestBuilder.js")
-const tParser             = require("../lib/tweetParser.js")
-const tweetParser         = tParser.tweetParser
-const singleTweetParser   = tParser.singleTweetParser;
-const twitterReq          = requestBuilder.twitterReq
+const requestBuilder    = require("../lib/requestBuilder.js")
+const tParser           = require("../lib/tweetParser.js")
+const tweetParser       = tParser.tweetParser
+const singleTweetParser = tParser.singleTweetParser;
+const twitterReq        = requestBuilder.twitterReq
 const defineParams      = requestBuilder.defineParams;
 const likeReq           = requestBuilder.likeReq;
 const likeAllTweets     = requestBuilder.likeAllTweets;
@@ -22,5 +22,30 @@ describe('defineParams', function(){
   it('defines the search parameters', function(){
     expect(defineParams('chocolate')).to.be.a('object').with.property('count')
     // expect(defineParams('chocolate')).to.have.property('count')
+  })
+});
+
+describe('twitterReq', function() {
+ it('returns the correct tweet data', function(done) {
+   var promise = twitterReq({
+     q: '#competition'
+   });
+   // console.log(promise.then().resolve())
+   promise.then().should.eventually.have.property('statuses').notify(done);
+ });
+});
+
+// describe('likeReq', function(){
+//   it('likes a tweet given the tweet_id', function(){
+//
+//   })
+// })
+
+describe('likeAllTweets', function(){
+  it('likes all returned tweets', function(){
+    var tweet1 = {id: 1234}
+    var tweet2 = {id: 5678}
+    likeAllTweets([tweet1, tweet2])
+    expect(tweet1.liked).to.be.true
   })
 })
